d9654fb1177e6cbab7a3a2ecbfcfb9aca0a7ff6f
avrojp <avrostu@gmail.com>
2024-06-29 03:53:38 +0600

    Dark/Light Mode Added Successfully

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,4 +11,33 @@ document.addEventListener("DOMContentLoaded", function () {
       navbarContent.classList.remove("show");
     }
   });
+
+  //Color Mode
+  const modeToggle = document.getElementById("mode-toggle");
+  const darkModeStylesheet = document.getElementById("dark-mode-stylesheet");
+  const lightModeStylesheet = document.getElementById("light-mode-stylesheet");
+
+  // Function to switch to dark mode
+  function switchToDarkMode() {
+    darkModeStylesheet.disabled = false;
+    lightModeStylesheet.disabled = true;
+  }
+
+  // Function to switch to light mode
+  function switchToLightMode() {
+    darkModeStylesheet.disabled = true;
+    lightModeStylesheet.disabled = false;
+  }
+
+  // Initialize with dark mode as default
+  switchToDarkMode();
+
+  // Event listener for checkbox change
+  modeToggle.addEventListener("change", function () {
+    if (this.checked) {
+      switchToLightMode(); // Switch to light mode
+    } else {
+      switchToDarkMode(); // Switch to dark mode
+    }
+  });
 });
